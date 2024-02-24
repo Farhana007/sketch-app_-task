9ca829d4223505927b16b0545e8870e5eb90eb82
Farhana <farhanafarhana043@gmail.com>
2024-02-24 11:02:40 -0500

    providers added in Main File

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:sketch_app/provider/sketch_provider.dart';
+import 'package:sketch_app/provider/sketch_setting.dart';
+import 'package:sketch_app/view/sketch_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      /// --- > This provider is For Undo,Redo,Clearing Cnavas Functionality <--- ///
+      ChangeNotifierProvider(
+        create: (_) => DrawingStateManager(),
+      ),
+
+      /// --- > This provider is For Changing Color and Stroke Width Functionality <--- ///
+      ChangeNotifierProvider(
+        create: (_) => DrawingSettings(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Sketch App',
+        debugShowCheckedModeBanner: false,
+        title: 'SketchApp',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Container(
-          color: Colors.red,
-        ));
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+            appBarTheme: const AppBarTheme(
+                iconTheme: IconThemeData(color: Colors.white))),
+        home: const SketchScreen());
   }
 }
