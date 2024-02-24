343e2ee42b66a334a3fdb7a66554b5a98ff077ee
Farhana <farhanafarhana043@gmail.com>
2024-02-24 14:22:34 -0500

    comments added in Sketch screen file

--- lib/view/sketch_screen.dart
+++ lib/view/sketch_screen.dart
@@ -7,16 +7,24 @@ import '../provider/sketch_provider.dart';
 import '../utils/color_change_function.dart';
 import '../utils/stroke_change_function.dart';
 
+///@Description:This screen Contains The UI of the app ,
+///@param:Theres is no parameters fro this screen
+///@function:There are Buttons for Undo, Redo, Clear Canvas, Color Choosing,Width Changing,Download
+///Provider as StateMangement
+
 class SketchScreen extends StatelessWidget {
   const SketchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Height and Widht variable
     double heightCount = MediaQuery.sizeOf(context).height;
     double widthCount = MediaQuery.sizeOf(context).width;
+    //--->Creating the Instance of Provider to call the functions <--- ///
     final drawingManager =
         Provider.of<DrawingStateManager>(context, listen: false);
 
+    /// --->Scaffold StartS here <--- ///
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
@@ -26,12 +34,16 @@ class SketchScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Center(child: "Sketch your thought out".text.white.make()),
+                Center(
+                    child: "Sketch your thought out".text.white.make()), //Title
                 10.heightBox,
+                // --> This Row Contains two part, one contains buttons for cuntion, and another one is canvas <--//
                 Row(
                   children: [
+                    // --->First Part of the Row <---///
                     Column(
                       children: [
+                        /**Button to Undo */
                         IconButton(
                             onPressed: () {
                               drawingManager.undo();
@@ -40,6 +52,8 @@ class SketchScreen extends StatelessWidget {
                               Icons.undo,
                               color: Colors.white,
                             )),
+
+                        /**Button to Redo */
                         IconButton(
                             onPressed: () {
                               drawingManager.redo();
@@ -48,6 +62,8 @@ class SketchScreen extends StatelessWidget {
                               Icons.redo,
                               color: Colors.white,
                             )),
+
+                        /**Button to Clear Canvas */
                         IconButton(
                             onPressed: () {
                               drawingManager.clearCanvas();
@@ -56,6 +72,8 @@ class SketchScreen extends StatelessWidget {
                               CupertinoIcons.clear_circled,
                               color: Colors.white,
                             )),
+
+                        /**Button to Showing Color chosing Dialog*/
                         IconButton(
                             onPressed: () {
                               showColorPickerDialog(context);
@@ -64,6 +82,8 @@ class SketchScreen extends StatelessWidget {
                               Icons.color_lens_rounded,
                               color: Colors.white,
                             )),
+
+                        /**Button to Showing  width chossing dialog*/
                         IconButton(
                             onPressed: () {
                               showStrokeWidthDialog(context);
@@ -77,11 +97,19 @@ class SketchScreen extends StatelessWidget {
                         .box
                         .size(widthCount * 0.25, heightCount * 0.9)
                         .color(const Color.fromARGB(255, 67, 47, 131))
+                        .border(
+                            color: const Color.fromARGB(255, 49, 4, 63),
+                            width: 2)
                         .make(),
+
+                    /// ---> Second part of the Row , THis is the canvas <--- ///
                     const SketchingCanvas()
                         .box
                         .size(widthCount * 0.7, heightCount * 0.9)
                         .white
+                        .border(
+                            color: const Color.fromARGB(255, 69, 21, 5),
+                            width: 2)
                         .make(),
                   ],
                 )
