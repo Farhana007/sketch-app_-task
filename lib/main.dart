6b25f615de2a6ff80b1f6c92b27c9e8f650ba896
Farhana <farhanafarhana043@gmail.com>
2024-03-22 21:53:39 -0400

    default orientation set to landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sketch_app/provider/download_provider.dart';
 import 'package:sketch_app/provider/sketch_provider.dart';
 import 'package:sketch_app/provider/sketch_setting.dart';
+import 'package:sketch_app/view/new_sketch_screen.dart';
 import 'package:sketch_app/view/sketch_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    //Making the default orientation Landscape
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]);
+
+  /// ---> Global Key for Provider <--- ///
   final GlobalKey canvasKey = GlobalKey();
   runApp(MultiProvider(
     providers: [
@@ -41,6 +52,6 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
             appBarTheme: const AppBarTheme(
                 iconTheme: IconThemeData(color: Colors.white))),
-        home: const SketchScreen());
+        home: const NewSketchScreen());
   }
 }
